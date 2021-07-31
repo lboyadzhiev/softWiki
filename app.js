403678b2d0df35a3f04cf1300db41c3bb10555a3
lboyadzhiev <l.boyadzhievv@abv.bg>
2021-07-31 21:22:50 +0300

    router

---
+++ app.js
@@ -0,0 +1,18 @@
+import page from './node_modules/page/page.mjs';
+
+import { homePage } from './views/home.js';
+import { createPage } from './views/create.js';
+import { detailsPage } from './views/details.js';
+import { editPage } from './views/edit.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
+
+page('/', homePage);
+page('/dashboard', homePage);
+page('/create', createPage);
+page('/details/:id', detailsPage);
+page('/edit/:id', editPage);
+page('/register', registerPage);
+page('/login', loginPage);
+
+page.start();
